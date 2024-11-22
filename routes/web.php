a1cbfc049c427ba8520cc302d46df78c45b50e9f
Suhendar70100 <suhendarteguh0@gmail.com>
2024-11-22 07:29:19 +0700

    added aplication documentation

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ProductController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\API\CarRentalController;
 use App\Http\Controllers\API\ListProductController;
 
 Route::get('/', [ProductController::class, 'index'])->name('product')->middleware(['isAdmin', 'auth']);
 Route::get('/list-product', [ListProductController::class, 'index'])->name('list-product');
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
