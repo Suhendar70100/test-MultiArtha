12b1d23bed64f01d8bdbd8f487c82b974ed00b2d
Suhendar70100 <suhendarteguh0@gmail.com>
2024-11-22 09:13:30 +0700

    adding comments to routes

--- routes/api.php
+++ routes/api.php
@@ -10,12 +10,13 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::as('api.')->group(function () {
+    // tugas 2
     Route::get('/product', [ProductController::class, 'dataTable'])->name('product.dataTable');
     Route::post('/product', [ProductController::class, 'store'])->name('product.store');
     Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
     Route::put('/product/{id}', [ProductController::class, 'update'])->name('product.update');
     Route::delete('/product/{id}', [ProductController::class, 'delete'])->name('product.delete');
 
-    // api
+    // tugas 3
     Route::apiResource('/people', PeopleController::class);
 });
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\ListProductController;
 
+// tugas 2
 Route::get('/', [ProductController::class, 'index'])->name('product')->middleware(['isAdmin', 'auth']);
+
+// tugas 5
 Route::get('/list-product', [ListProductController::class, 'index'])->name('list-product');
 
 require __DIR__.'/auth.php';
